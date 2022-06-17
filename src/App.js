1c233c7d17f9dc75fabfa6af1a8cf7cfa1784f07
Dayana <200103264@stu.sdu.edu.kz>
2022-06-17 13:38:48 +0600

    add all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
+import sky from './01.jpeg';
+import lake from './02.jpg';
+import mountain from './03.jpeg';
+import fox from './04.jpeg';
+import clouds from './05.jpeg'
 
 function App() {
+  const user ={
+    firstname:'Joe',
+    lastName:'Biden',
+  }
+  
+  const blocks={
+    width:"20%",
+    height: "100vh",
+    overflow:"hidden",
+    position:"relative"
+
+  }
+  const getUserName = ({firstname , lastName}) =>{
+    return ` ${firstname} ${lastName}`
+  }
+  
+  const age = 20;
+  const condRendering = age>=18 ? <div>Adult</div>:<div>Teen</div>
+  const firstText = <h1>First Text</h1>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="divstyle">
+      <div>
+        <img src={sky}/>
+        <p>HEY</p>
+      </div>
+      <div>
+        <img src={lake}/>
+        <p>LET'S</p>
+      </div>
+      <div>
+        <img src={mountain}/>
+        <p>GIVE</p>
+      </div>
+      <div>
+        <img src={fox}/>
+        <p>ALL</p>
+      </div>
+      <div>
+        <img src={clouds}/>
+        <p>YOU CAN</p>
+      </div>
     </div>
   );
 }
